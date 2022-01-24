4ab0a2a98c9658db7dbc50e8f22c516de0a90b2b
Yougraj <yougrajbora.developer@gmail.com>
2022-01-24 12:21:46 +0530

    Gotham series add to this libery

---
+++ gothamS1.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+season_1() {
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E01%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E02%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E03%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E04%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E05%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E06%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E07%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E08%20(O2TvSeries.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E09%20(O2TvSeries.Com).mp4"
+  for i in {10..22}
+  do
+    wget "http://d12.o2tv.org/Gotham/Season%2001/Gotham%20-%20S01E$i%20(O2TvSeries.Com).mp4"
+  done
+}
+
+season_2() {
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E01%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E02%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E03%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E04%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E05%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E06%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E07%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E08%20(TvShows4Mobile.Com).mp4"
+  wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E09%20(TvShows4Mobile.Com).mp4" 
+  for i in {10..22}
+  do
+    wget "http://d9.o2tv.org/Gotham/Season%2002/Gotham%20-%20S02E$i%20(TvShows4Mobile.Com).mp4"
+  done
+}
+
+
+season_3() {
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E01%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E02%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E03%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E04%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E05%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E06%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E07%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E08%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E09%20HD%20(TvShows4Mobile.Com).mp4"
+  for i in {1..22}
+  do
+    wget "http://d4.o2tv.org/Gotham/Season%2003/Gotham%20-%20S03E$i%20HD%20(TvShows4Mobile.Com).mp4"
+  done
+}
+
+season_4() {
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E01%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E02%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E03%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E04%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E05%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E06%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E07%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E08%20HD%20(TvShows4Mobile.Com).mp4"
+  wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E09%20HD%20(TvShows4Mobile.Com).mp4"
+  for i in {1..22}
+  do
+    wget "http://d3.o2tv.org/Gotham/Season%2004/Gotham%20-%20S04E$i%20HD%20(TvShows4Mobile.Com).mp4"
+  done
+}
+
+season_5() {
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E01%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E02%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E03%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E04%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E05%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E06%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E07%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E08%20(TvShows4Mobile.Com).mp4"
+  wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E09%20(TvShows4Mobile.Com).mp4"
+  for i in {1..12}
+  do
+    wget "http://d12.o2tv.org/Gotham/Season%2005/Gotham%20-%20S05E$i%20(TvShows4Mobile.Com).mp4"
+  done
+}
+
+
+read -p "Which season you want to download 'Gotham'(1, 2, 3, 4, 5,): " input
+
+if [ $input == 1 ]
+then
+    season_1
+
+elif [ $input == 2 ]
+then
+    season_2
+
+elif [ $input == 3 ]
+then
+    season_3
+elif [ $input == 4 ]
+then
+    season_4
+elif [ $input == 5 ]
+then
+    season_5
+else
+    echo "OK"
+fi
